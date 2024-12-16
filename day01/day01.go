1fee125547372a146b80532ab60ec0db2fd77123
bdnugget <bdbosman@gmail.com>
2024-12-16 16:11:42 +0100

    Day 1 part 1

--- day01/day01.go
+++ day01/day01.go
@@ -1,14 +1,13 @@
 package day01
 
 import (
-	"fmt"
 	"os"
 	"strings"
+	"strconv"
+	//"fmt"
+	"slices"
+	"math"
 )
-
-func main(){
-	fmt.Println(InputLines("input"))
-}
 
 func InputLines(filename string) []string {
 	b, err := os.ReadFile(filename)
@@ -18,6 +17,38 @@ func InputLines(filename string) []string {
 	return strings.Split(string(b), "\n")
 }
 
+func atoi(s string) int {
+	v, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
 func Part01() int {
-	return 69
+	lines := InputLines("./day01/input")
+	var leftCol, rightCol []int
+	for _, line := range lines {
+		left, right, found := strings.Cut(line, "   ")
+		if found {
+			leftCol = append(leftCol, atoi(left))
+			rightCol = append(rightCol, atoi(right))
+		}
+	}
+
+	slices.Sort(leftCol)
+	slices.Sort(rightCol)
+
+	var diffCols []int
+	for i, val := range leftCol {
+		diffCols = append(diffCols, int(math.Abs(float64((val - rightCol[i])))))
+	}
+
+	sumDiff := 0
+	for _, diff := range diffCols {
+		sumDiff += diff
+	}
+
+	//fmt.Printf("%v", sumDiff)
+	return sumDiff
 }
