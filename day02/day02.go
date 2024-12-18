09738f36d77784cdec6a21c90f5560ab40dba450
bdnugget <bdbosman@gmail.com>
2024-12-19 00:38:36 +0100

    Day 2 part 1

--- day02/day02.go
+++ day02/day02.go
@@ -1,17 +1,39 @@
 package day02
 
 import (
-	"fmt"
+	_ "fmt"
+	"slices"
+	"strings"
 
 	"github.com/bdnugget/AoC_2024/utils"
 )
 
 func Part01() int {
-	reports := utils.InputLines("./day02/input")
+	reportLines := utils.InputLines("./day02/input")
+	var reports [][]int
+	for _, report := range reportLines {
+		levels := strings.Fields(report)
+		var row []int
+		for i, level := range levels {
+			levelInt := utils.MustAtoi(level)
+			if i == 0 {
+				row = append(row, levelInt)
+			} else if i > 0 && utils.AbsInt(levelInt-row[i-1]) >= 1 && utils.AbsInt(levelInt-row[i-1]) <= 3 {
+				row = append(row, levelInt)
+			} else {
+				row = nil
+				break
+			}
+		}
+		if row == nil {
+			continue
+		}
+		if slices.IsSorted(row) || slices.IsSortedFunc(row, func(a, b int) int { return b - a }) {
+			reports = append(reports, row)
+		}
 
-	fmt.Println(reports[1])
-
-	return 420
+	}
+	return len(reports)
 }
 
 func Part02() int {
